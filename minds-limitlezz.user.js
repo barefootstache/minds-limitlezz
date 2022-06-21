88b75ca1e932f4f8aeb8c72a0d7caa0428a5eeb2
barefootstache <barefootstache@protonmail.com>
2022-06-21 02:22:32 +0200

    update meta

--- minds-limitlezz.user.js
+++ minds-limitlezz.user.js
@@ -7,6 +7,8 @@
 // @include       https://www.minds.com/*
 // @copyright     2022, barefootstache (https://openuserjs.org/users/barefootstache)
 // @license       MIT
+// @updateURL     https://openuserjs.org/meta/barefootstache/Minds_Limitlezz.meta.js
+// @downloadURL   https://openuserjs.org/install/barefootstache/Minds_Limitlezz.user.js
 // ==/UserScript==
 
 (function () {
